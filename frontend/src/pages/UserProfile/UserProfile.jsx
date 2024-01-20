58c7d248ecf11c1c858e3bef21d611d579b0553d
sssSync <soumyazesan@gmail.com>
2024-01-20 15:24:15 +0530

    profile-some

--- frontend/src/pages/UserProfile/UserProfile.jsx
+++ frontend/src/pages/UserProfile/UserProfile.jsx
@@ -4,16 +4,19 @@ const UserProfile = () => {
     <div className="flex justify-center items-center h-screen bg-[#161618]">
       <div id="profile-div">
         <div className="self-info">
-          <div id="profile-pic" className="bg-white"></div>
+          <div id="profile-pic" className=" flex justify-center items-center ">
+            S
+          </div>
+
           <div id="contact-info">
             <div className="contact-info-writing">
-              <p className="name">Mr. Hanra</p>
-              <p className="contact">
+              <h1 className="name mb-4">Sayan Hanra </h1>
+              <h3 className="det">
                 Contact No : <span>6464654656</span>
-              </p>
-              <p className="address">
+              </h3>
+              <h3 className="det">
                 Address : <span> Sorisa,Kolkata ,WB</span>
-              </p>
+              </h3>
             </div>
             <div className="contact-info-operations">
               <button className="op-btn" id="add-room"></button>
@@ -22,9 +25,7 @@ const UserProfile = () => {
             </div>
           </div>
         </div>
-        <div className="room-info">
-          <div id="room-details"></div>
-        </div>
+        <div className="room-info"></div>
       </div>
     </div>
   );
